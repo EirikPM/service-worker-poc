eb5b2d19d4c39671729e7a9cc25b9d387b4df9c3
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2022-10-18 09:56:39 +0200

    Ejected out of create-app

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -23,3 +23,7 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
 export default store
+
+store.subscribe(() => {
+    saveObservasjon(store.getState())
+})
